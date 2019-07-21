3b19a78931f9e4ab10ec6340eeb49019d94160b9
Shukla,Shubham <shubham.cored@gmail.com>
2019-07-21 14:50:35 +0530

    aws connect changes

--- demo-2/vars.tf
+++ demo-2/vars.tf
@@ -5,24 +5,24 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-1 = "ami-026c8acd92718196b"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
   }
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "c:\\users\\shushukl\\.ssh\\id_rsa"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "c:\\users\\shushukl\\.ssh\\id_rsa.pub"
 }
 
 variable "INSTANCE_USERNAME" {
